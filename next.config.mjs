904ca71e8e1f7963f59572a91cd140acd54761d3
fernandoverago <fernando.verago@energycap.com>
2025-07-22 11:20:05 +0100

    Add deploy settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
+  // Add this if your repo name is not your username
+  basePath: '/shopping-list-app',
+  assetPrefix: '/shopping-list-app/',
 }
 
 export default nextConfig
